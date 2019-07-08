0ed1abef2b7a1cb73fa672540ef552ec830261b0
ahmdalgendi <ahmd.algendi@gmail.com>
2019-07-08 20:37:19 +0200

    edit added no image edit

--- include/vars.php
+++ include/vars.php
@@ -4,8 +4,8 @@ $host = 'localhost';
 $user = 'root';
 $pass = '';
 $db   = 'users';
-$email = 'ahmd.algendi@gmail.com';
-$email_pass = "Ou76G4@1fOwA#FlG08iWvoIey";
-// hello
+$email = 'yourGmail@gmail.com';
+$email_pass = "Your_gamil_password";
+
 ?>
 
